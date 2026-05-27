2ce0001121540d00ee81d22d8a36a7f29a42bbe5
Vincent Chen <chen.vincent.ny@gmail.com>
2026-05-27 19:29:29 -0400

    fixing clarity, also fixed length slider not working

--- ballistic.py
+++ ballistic.py
@@ -39,6 +39,39 @@ gr = graph(align='right', title="<b> Energy of Pendulum vs Time<b>", ytitle="Ene
 my_curve = gcurve(color=color.red, label="Kinetic Energy")
 my_curve2 = gcurve(color=color.blue, label="Potential Energy")
 
+
+### ALL THE BINDS
+
+def massBallChange(s):
+    global mBall
+    mBall = s.value
+    ball_text.text = f"{mBall:.3f} kg"
+
+def massBulletChange(s):
+    global mBullet
+    mBullet = s.value
+    mass_text.text = f"{mBullet:.3f} kg"
+
+def speedBulletChange(s):
+    global vBullet
+    vBullet = vector(s.value,0,0)
+    speed_text.text = f"{s.value:.1f} m/s"
+
+
+def lenChange(s):
+    if(mode==0):
+        global L
+        L = s.value
+        length_text.text = f"{s.value:.1f} m"
+
+
+def my_action(b):
+    global mode
+    if(b.text == "2D"):
+        mode = 1
+    else:
+        mode = 0
+
 # 2. Create the button
 button(bind=my_action, text="1D", pos=scene.caption_anchor)
 button(bind=my_action, text="2D", pos=scene.caption_anchor)
@@ -50,51 +83,31 @@ mass_text = wtext(text=f"{mBullet:.3f} kg")
 
 slider_mass_bullet = slider( bind=massBulletChange, min=0,max=0.1,value=mBullet, length=250, step=.001)
 
-def massBulletChange(s):
-    global mBullet
-    mBullet = s.value
-    mass_text.text = f"{mBullet:.3f} kg"
-
 ### BALL MASS !!
 scene.append_to_caption("\n\nBall Mass: ")
 ball_text = wtext(text=f"{mBall:.3f} kg")
 
 slider_mass_ball = slider( bind=massBallChange, min=0, max=50, value=mBall, length=250)
 
-def massBallChange(s):
-    global mBall
-    mBall = s.value
-    ball_text.text = f"{mBall:.3f} kg"
-
 ### BULLET START VEL.
 scene.append_to_caption("\n\nBullet Speed: ")
 
 speed_text = wtext(text=f"{vBullet.x:.3f} m/s")
 
-slider_bullet_speed = slider( bind=speedBulletChange, min=50, max=1000, value=vBullet, length=250, step=50)
-
-def speedBulletChange(s):
-    global vBullet
-    vBullet = vector(s.value,0,0)
-    speed_text.text = f"{s.value:.1f} m/s"
+slider_bullet_speed = slider( bind=speedBulletChange, min=50, max=1000, value=vBullet.x, length=250, step=50)
 
 ### LENGTH FOR PENDULUM
+scene.append_to_caption("\n\nPendulum Length: ")
 
-if(mode == 0):
-    scene.append_to_caption("\n\nPendulum Length: ")
-    
-    length_text = wtext(text=f"{L:.3f} m")
-    
-    slider_length_speed = slider( bind=speedBulletChange, min=0, max=10, value=vBullet, length=250, step=.5)
-    
-    def lenChange(s):
-        global L
-        L = s.value
-        length_text.text = f"{s.value:.1f} m/s"
+length_text = wtext(text=f"{L:.3f} m")
+
+slider_length = slider( bind=lenChange, min=0, max=10, value=L, length=250, step=.5)
+
 
 sleep(2)
 t=0
 while True:
+    ### sliders on display  
     rate(frameRate)
     if(bulletShot):
         if(bulletFirstShot):
@@ -110,7 +123,6 @@ while True:
         my_curve.plot(t, (mBall+mBullet)*(L*omega)*(L*omega)/2) # kinetic energy
         my_curve2.plot(t, (mBullet)*vBulletStart*vBulletStart/2-(mBall+mBullet)*(L*omega)*(L*omega)/2) # potential energy
         t += dt
-    
 
 def getNewSpeed(ballVelocity, ballMass, bulletVelocity, bulletMass):
     newMomentum = calculate2DMomentum(ballVelocity, ballMass, bulletVelocity, bulletMass)
@@ -125,10 +137,3 @@ def calculate2DMomentum(ballVelocity, ballMass, bulletVelocity, bulletMass):
     
 def calculate1DMomentum(ballSpeed, ballMass, bulletSpeed, bulletMass):
     return(ballSpeed*ballMass + bulletSpeed*bulletMass)
-
-
-def my_action(b):
-    if(b.text == "2D"):
-        mode = 1
-    else:
-        mode = b.text
