f2c8c24abd9e497e4e3dcfc46177b37084d6f9a7
Vincent Chen <chen.vincent.ny@gmail.com>
2026-06-09 15:26:14 -0400

    added bullet angle to fix issues with slider, merged 1d and 2d successfully, working on touch-ups
    Refactor ballistic simulation code to improve structure and readability. Added new parameters for bullet angle and updated the main loop logic.

--- ballistic.py
+++ ballistic.py
@@ -1,17 +1,26 @@
 Web VPython 3.2
 from vpython import *
 
-################### basic parameters
-L = 2
-g = 9.81
+## DO NOT CHANGE
 
 scene.align = 'left'
 scene.width = 400
 scene.height = 280
 scene.center=vec(0,2.5,0)
-scene.userzoom = True
-scene.userspin = True
-scene.userpan = True
+running = False
+
+frameRate = 10000
+dt = 1/frameRate
+
+bulletShot = True
+bulletFirstShot = True
+
+################### basic parameters
+L = 2
+bulletAngle = 90
+g = 9.81
+
+a = vec(0, -g, 0)
 mode = 0
 
 mBall= 1
@@ -20,15 +29,19 @@ vBall = vector(0, 0, 0)
 mBullet = 0.002
 vBullet = vector(1000, 0, 0)
 vBulletStart = 1000
-bulletShot = True
-bulletFirstShot = True
 
 theta = pi/2
 omega = 0
-frameRate = 10000
-dt = 1/frameRate
 
 transitionY = L/2
+pivot = vector(0, transitionY, 0)
+pBall = pivot + vector(1,-sqrt(L**2-1),0)
+
+gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
+my_curve = gcurve(color=color.red, label="Kinetic Energy")
+my_curve2 = gcurve(color=color.blue, label="Potential Energy")
+
+##### AXES
 
 x=arrow(pos=vector(0,0,0), axis=vector(5,0,0), color=color.red, shaftwidth=0.1)
 label(pos=x.pos + x.axis, text='x', xoffset=20, height=16)
@@ -39,13 +52,10 @@ label(pos=y.pos + y.axis, text='y', yoffset=20, height=16)
 z=arrow(pos=vector(0,0,0), axis=vector(0,5,0), color=color.green, shaftwidth=0.1)
 label(pos=z.pos + z.axis, text='z', zoffset=20, height=16)
 
-ball = sphere(pos=vector(L*cos(theta),transitionY-L*sin(theta)+L/2,0), radius = 0.25, color = color.orange)
+##########
+
+ball = sphere(pos=vector(L*cos(theta),-L*sin(theta)+L,0), radius = 0.25, color = color.orange)
 rod = cylinder(pos=vec(0, transitionY+L/2, 0), axis=ball.pos-vec(0, transitionY+L/2, 0), color=color.orange, radius = 0.03, length = L)
-
-gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
-my_curve = gcurve(color=color.red, label="Kinetic Energy")
-my_curve2 = gcurve(color=color.blue, label="Potential Energy")
-
 
 ### ALL THE BINDS
 
@@ -55,7 +65,6 @@ def massBallChange(s):
     mBall = s.value
     ball_text.text = f"{mBall:.3f} kg"
     ball.radius = 0.25 * (s.value**(1/3))
-
 
 def massBulletChange(s):
     global mBullet
@@ -67,57 +76,98 @@ def speedBulletChange(s):
     vBullet = vector(s.value,0,0)
     speed_text.text = f"{s.value:.1f} m/s"
 
-
 def lenChange(s):
     global mode
-#    global length_label
-#    global length_text
-#    global slider_length
-    if(mode==0):
-        global L
+    global L
+    global transitionY
+    global slider_length
+    global bulletAngle
+    if mode == 0:
         L = s.value
         length_text.text = f"{s.value:.1f} m"
         update_transitionY(s)
-        rod.pos.y = transitionY+L/2
+        rod.pos.y = transitionY + L/2
         rod.length = L
-        rod.axis = ball.pos - vec(0, transitionY+L/2, 0)
+        rod.axis = ball.pos - vec(0, transitionY + L/2, 0)
     else:
-        length_label.text = "Angle of Bullet to (+) x-axis: "
-        L = s.value()
+        bulletAngle = s.value
         length_text.text = f"{s.value:.1f} degrees"
-        ## fix transition?
-##change bullet angle to allow 0 to 180
 
 def my_action(b):
     theta = pi/2
     omega = 0
     global mode
+    global L
+    global bulletAngle
+    global slider_length
+    global pBall
+    global vBall
+    global mBall
+    global transitionY
     if(b.text == "2D"):
         mode = 1
         length_label.text = "Angle of Bullet to x-z plane: "
-        length_text.text = f"{L:.3f} degrees"
         slider_length.delete()
-        L = 90
-        slider_length.visible = True
-        slider_length = slider( bind=lenChange, min=45, max=135, value=L, length=250, step=5)
+        bulletAngle = 90
+        slider_length = slider( bind=lenChange, min=45, max=135, value=bulletAngle, length=250, step=5)
+        length_text.text = f"{bulletAngle:.1f} degrees"
+        pivot = vector(0, transitionY, 0)
+        mBall= 0.25
+        pBall = pivot + vector(1,-sqrt(L**2-1),0)
+        vBall = vector(0,0,10)
     else:
         mode = 0
         length_label.text = "Pendulum Length: "
-        length_text.text = f"{L:.3f} m"
-        L = 4
+        L = 2
         slider_length.delete()
-        slider_length.visible = True
         slider_length = slider( bind=lenChange, min=0.5, max=10, value=L, length=250, step=.5)
-    print("Switching to the following number of degrees of freedom:", mode+1)
+        length_text.text = f"{L:.1f} m"
+        
+    reset()
+    main_loop(b)
+
+def reset():
+    global running = False
+    global a = vec(0, -g, 0)
+    
+    global mBall= 1
+    global vBall = vector(0, 0, 0)
+    
+    global mBullet = 0.002
+    global vBullet = vector(1000, 0, 0)
+    global vBulletStart = 1000
+    
+    global theta = pi/2
+    global omega = 0
+    
+    global transitionY = 1
+    global pivot = vector(0, transitionY, 0)
+    global pBall = pivot + vector(1,-sqrt(2**2-1),0)
+    
+    global gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
+    global my_curve = gcurve(color=color.red, label="Kinetic Energy")
+    global my_curve2 = gcurve(color=color.blue, label="Potential Energy")
+    
+    global ball = sphere(pos=vector(L*cos(theta),-L*sin(theta)+L,0), radius = 0.25, color = color.orange)
+    global rod = cylinder(pos=vec(0, transitionY+L/2, 0), axis=ball.pos-vec(0, transitionY+L/2, 0), color=color.orange, radius = 0.03, length = L)
+
+
+    
 
 def update_transitionY(b):
     if(mode ==0):
         global transitionY
         transitionY = b.value/2
 
-# 2. Create the button
+# 2. Create the buttons
 button(bind=my_action, text="1D", pos=scene.caption_anchor)
 button(bind=my_action, text="2D", pos=scene.caption_anchor)
+
+
+scene.append_to_caption("\n\n")
+button(bind=main_loop, text="START", pos=scene.caption_anchor)
+button(bind=main_loop, text="STOP", pos=scene.caption_anchor)
+
 
 ### BULLET MASS !!
 scene.append_to_caption("\n\nBullet Mass: ")
@@ -140,11 +190,15 @@ length_label = wtext(text="Pendulum Length: ")
 length_text = wtext(text=f"{L:.1f} m")
 slider_length = slider( bind=lenChange, min=0.1, max=5, value=L, length=250, step=.1)
 
-running = False
-
 def main_loop(b):
     global running
+    global gr
+    global printlag
     if(b.text=='START'):
+        printlag.delete()
+        printlag = wtext(text="\n\n\n\n\n\n")
+        gr.delete()
+        gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
         slider_length.disabled = True
         slider_bullet_speed.disabled = True
         slider_mass_ball.disabled = True
@@ -158,18 +212,14 @@ def main_loop(b):
         running = False
             
 
-scene.append_to_caption("\n\n")
-button(bind=main_loop, text="START", pos=scene.caption_anchor)
-button(bind=main_loop, text="STOP", pos=scene.caption_anchor)
 
-scene.append_to_caption("\n\n\n\n\n\n")
 
+printlag = wtext(text="\n\n\n\n\n\n")
 t=0        
 while True:
     rate(frameRate)
     if(running):
         if(mode==0):
-            ### sliders on display  
             rate(frameRate)
             if(bulletShot):
                 if(bulletFirstShot):
@@ -188,8 +238,44 @@ while True:
                 my_curve2.plot(t, (mBullet+mBall)*g*(L-L*sin(theta))) # potential energy
                 t += dt
         else:
-            sleep(5) #fix to be correct
-    # if not running, put smth here
+            rate(frameRate)
+
+            if(bulletShot):
+                if(bulletFirstShot):
+                    vBall = (mBall*vBall + mBullet*vBullet)/(mBall+mBullet)
+                    bulletFirstShot = False
+
+                a = vector(0,-g,0)
+
+                r = pBall - pivot
+                rHat = norm(r)
+
+                aRadial = dot(a,rHat)*rHat
+                aTangent = a - aRadial
+
+                vBall += aTangent*dt
+
+                vRadial = dot(vBall,rHat)*rHat
+                vBall -= vRadial
+
+                pBall += vBall*dt
+
+                r = pBall - pivot
+                rHat = norm(r)
+                pBall = pivot + L*rHat
+
+                ball.pos = pBall
+                rod.pos = pivot
+                rod.axis = pBall - pivot
+
+                KE = 0.5*(mBall+mBullet)*mag(vBall)**2
+                PE = (mBall+mBullet)*g*(pBall.y -ball.radius- (pivot.y - L))
+
+                my_curve.plot(t, KE)
+                my_curve2.plot(t, PE)
+
+                t += dt
+
 def getNewSpeed(ballVelocity, ballMass, bulletVelocity, bulletMass):
     newMomentum = calculate2DMomentum(ballVelocity, ballMass, bulletVelocity, bulletMass)
     totalMass = ballMass + bulletMass
