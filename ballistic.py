accc6a9e790f3d93c3abccf77629e3347b4daf63
Vincent Chen <chen.vincent.ny@gmail.com>
2026-06-09 17:41:59 -0400

    Enhance ballistic simulation with reset functionality
    Updated the ballistic simulation to include a reset button and improved bullet trajectory calculations.

--- ballistic.py
+++ ballistic.py
@@ -11,6 +11,7 @@ running = False
 
 frameRate = 10000
 dt = 1/frameRate
+t=0
 
 bulletShot = True
 bulletFirstShot = True
@@ -34,12 +35,12 @@ theta = pi/2
 omega = 0
 
 transitionY = L/2
-pivot = vector(0, transitionY, 0)
+pivot = vector(0, transitionY+L/2, 0)
 pBall = pivot + vector(1,-sqrt(L**2-1),0)
 
 gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
-my_curve = gcurve(color=color.red, label="Kinetic Energy")
-my_curve2 = gcurve(color=color.blue, label="Potential Energy")
+my_curve = gcurve(graph = gr, color=color.red, label="Kinetic Energy")
+my_curve2 = gcurve(graph = gr, color=color.blue, label="Potential Energy")
 
 ##### AXES
 
@@ -91,7 +92,10 @@ def lenChange(s):
         rod.axis = ball.pos - vec(0, transitionY + L/2, 0)
     else:
         bulletAngle = s.value
+        angleRad = bulletAngle * pi / 180
+        vBall = vector(vBulletStart * cos(angleRad), 0, vBulletStart * sin(angleRad))
         length_text.text = f"{s.value:.1f} degrees"
+
 
 def my_action(b):
     theta = pi/2
@@ -104,17 +108,22 @@ def my_action(b):
     global vBall
     global mBall
     global transitionY
+    global printlag
+    
+    global t = 0
     if(b.text == "2D"):
         mode = 1
         length_label.text = "Angle of Bullet to x-z plane: "
         slider_length.delete()
         bulletAngle = 90
-        slider_length = slider( bind=lenChange, min=45, max=135, value=bulletAngle, length=250, step=5)
+        slider_length = slider( bind=lenChange, min=0, max=180, value=bulletAngle, length=250, step=5)
         length_text.text = f"{bulletAngle:.1f} degrees"
         pivot = vector(0, transitionY, 0)
         mBall= 0.25
         pBall = pivot + vector(1,-sqrt(L**2-1),0)
-        vBall = vector(0,0,10)
+        angleRad = bulletAngle * pi / 180
+        vBullet = vector(vBulletStart * cos(angleRad), 0, vBulletStart * sin(angleRad))
+        vBall = vector(0, 0, 10)
     else:
         mode = 0
         length_label.text = "Pendulum Length: "
@@ -123,19 +132,30 @@ def my_action(b):
         slider_length = slider( bind=lenChange, min=0.5, max=10, value=L, length=250, step=.5)
         length_text.text = f"{L:.1f} m"
         
+    printlag.delete()
+    printlag = wtext(text="\n\n\n\n\n\n")
+    
     reset()
     main_loop(b)
 
 def reset():
+    global ball
+    global rod
     global running = False
     global a = vec(0, -g, 0)
+    global t = 0
     
-    global mBall= 1
+    global L = 2
+    global bulletAngle = 90
+    global g = 9.81
+    
+    global mBall = 1
     global vBall = vector(0, 0, 0)
     
     global mBullet = 0.002
     global vBullet = vector(1000, 0, 0)
     global vBulletStart = 1000
+    global bulletAngle = 90
     
     global theta = pi/2
     global omega = 0
@@ -144,15 +164,16 @@ def reset():
     global pivot = vector(0, transitionY, 0)
     global pBall = pivot + vector(1,-sqrt(2**2-1),0)
     
-    global gr = graph(align='left', title="<b> Energy of Pendulum vs Time<b>", ytitle="Energy [J]", xtitle="Time [s]", xmin=0,ymin=0, width=400, height=280)
-    global my_curve = gcurve(color=color.red, label="Kinetic Energy")
-    global my_curve2 = gcurve(color=color.blue, label="Potential Energy")
+    global my_curve
+    global my_curve2
+    my_curve.data = []
+    my_curve2.data = []
     
-    global ball = sphere(pos=vector(L*cos(theta),-L*sin(theta)+L,0), radius = 0.25, color = color.orange)
-    global rod = cylinder(pos=vec(0, transitionY+L/2, 0), axis=ball.pos-vec(0, transitionY+L/2, 0), color=color.orange, radius = 0.03, length = L)
-
-
-    
+    ball.pos = vector(L*cos(theta), -L*sin(theta) + L, 0)
+    ball.radius = 0.25 * (mBall**(1/3))
+    rod.pos = vec(0, transitionY + L/2, 0)
+    rod.length = L
+    rod.axis = ball.pos - vec(0, transitionY + L/2, 0)
 
 def update_transitionY(b):
     if(mode ==0):
@@ -167,7 +188,7 @@ button(bind=my_action, text="2D", pos=scene.caption_anchor)
 scene.append_to_caption("\n\n")
 button(bind=main_loop, text="START", pos=scene.caption_anchor)
 button(bind=main_loop, text="STOP", pos=scene.caption_anchor)
-
+button(bind=main_loop, text="RESET", pos=scene.caption_anchor)
 
 ### BULLET MASS !!
 scene.append_to_caption("\n\nBullet Mass: ")
@@ -190,10 +211,12 @@ length_label = wtext(text="Pendulum Length: ")
 length_text = wtext(text=f"{L:.1f} m")
 slider_length = slider( bind=lenChange, min=0.1, max=5, value=L, length=250, step=.1)
 
+
 def main_loop(b):
     global running
     global gr
     global printlag
+    global t
     if(b.text=='START'):
         printlag.delete()
         printlag = wtext(text="\n\n\n\n\n\n")
@@ -210,12 +233,14 @@ def main_loop(b):
         slider_mass_ball.disabled = False
         slider_mass_bullet.disabled = False
         running = False
+        if(b.text=='RESET'):
+            reset()
+            t=0
             
 
 
 
-printlag = wtext(text="\n\n\n\n\n\n")
-t=0        
+printlag = wtext(text="\n\n\n\n\n\n")      
 while True:
     rate(frameRate)
     if(running):
@@ -269,7 +294,7 @@ while True:
                 rod.axis = pBall - pivot
 
                 KE = 0.5*(mBall+mBullet)*mag(vBall)**2
-                PE = (mBall+mBullet)*g*(pBall.y -ball.radius- (pivot.y - L))
+                PE = (mBall+mBullet)*g*(pBall.y - (pivot.y - L)) ## find correction factor since PE != 0
 
                 my_curve.plot(t, KE)
                 my_curve2.plot(t, PE)
